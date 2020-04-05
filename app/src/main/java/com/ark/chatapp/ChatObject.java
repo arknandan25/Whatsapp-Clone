491fe0026c31fcab92883a27ec0b7bf1513a9132
ark <arkchauhan10@gmail.com>
2020-04-05 15:04:00 +0100

    first

---
+++ app/src/main/java/com/ark/chatapp/ChatObject.java
@@ -0,0 +1,13 @@
+package com.ark.chatapp;
+
+public class ChatObject {
+    public String chatId;
+
+    public ChatObject(String chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getChatId() {
+        return chatId;
+    }
+}
